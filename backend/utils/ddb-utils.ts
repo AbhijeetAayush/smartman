4bb3c448d09c70d7e26ad5da5855e2f545c91329
abhijeet <abhijeetayush899@gmail.com>
2025-07-05 01:47:54 +0530

    github Auth setup

--- backend/utils/ddb-utils.ts
+++ backend/utils/ddb-utils.ts
@@ -1,10 +1,10 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import {
-  DynamoDBDocumentClient,
-  GetCommand,
-  GetCommandOutput,
-  PutCommand,
-  PutCommandOutput,
+    DynamoDBDocumentClient,
+    GetCommand,
+    GetCommandOutput,
+    PutCommand,
+    PutCommandOutput,
 } from '@aws-sdk/lib-dynamodb';
 import { Item } from '../types/model';
 
@@ -12,14 +12,14 @@ const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
 export const saveItem = async <T extends Item>(tableName: string, item: T): Promise<PutCommandOutput> => {
-  return docClient.send(new PutCommand({ TableName: tableName, Item: item }));
+    return docClient.send(new PutCommand({ TableName: tableName, Item: item }));
 };
 
 export const getItem = async (tableName: string, key: { PK: string; SK: string }): Promise<GetCommandOutput> => {
-  try {
-    return await docClient.send(new GetCommand({ TableName: tableName, Key: key }));
-  } catch (error) {
-    console.error('Error fetching item from DynamoDB:', error);
-    throw error;
-  }
+    try {
+        return await docClient.send(new GetCommand({ TableName: tableName, Key: key }));
+    } catch (error) {
+        console.error('Error fetching item from DynamoDB:', error);
+        throw error;
+    }
 };
